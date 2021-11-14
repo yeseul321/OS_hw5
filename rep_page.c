1528ae8428c713ad646e2e0605ee2ad741dec991
yeseul321 <amy7806@gmail.com>
2021-11-14 23:25:25 +0900

    LRU complete

--- rep_page.c
+++ rep_page.c
@@ -19,7 +19,89 @@ void readInput(int *frames, int *pages, int *referenceString){
 	fclose(fp);
 }
 
-int lru_page(int frames, int page, int *referenceString){
+int lru_page(int frames, int pages, int *referenceString){
+	int temp[frames];
+	int frame[frames];
+	int pageFaults = 0, m, n, s;
+	char fault;
+
+	//printf("frames : %d, pages : %d\n", frames, pages);
+	printf("Used method : LRU\n");
+	printf("page reference string : ");
+	for(int m = 0; m < pages; m++)
+      	{
+           printf("%d ", referenceString[m]);
+      	}
+	printf("\n\n");
+	printf("\tframe   ");
+	for(int m = 1; m<=frames;m++){
+		printf("%d\t",m);
+	}
+	printf("page fault\ntime\n");
+
+	for(m = 0; m < frames; m++)
+      {
+            temp[m] = -1;
+      }
+      for(m = 0; m < pages; m++)
+      {
+            s = 0;
+	    fault = 'F';
+            for(n = 0; n < frames; n++)
+            {
+                  if(referenceString[m] == temp[n])
+                  {
+                        s++;
+                        pageFaults--;
+			fault = ' ';
+			frame[n] = m;
+                  }
+            }
+            pageFaults++;
+            if((pageFaults <= frames) && (s == 0))
+            {
+                  temp[pageFaults-1] = referenceString[m];
+		  frame[pageFaults-1] = m;
+            }
+            else if(s == 0)
+            {
+                  //temp[(pageFaults - 1) % frames] = referenceString[m];
+		  if(frame[0]<=frame[1]){
+			  if(frame[0]<=frame[2]){
+				  temp[0] = referenceString[m];
+				  frame[0] = m;
+			  }
+			  else{
+				  temp[2] = referenceString[m];
+				  frame[2] = m;
+			  }
+		  } 
+		  else{
+			  if(frame[1]<=frame[2]){
+				  temp[1] = referenceString[m];
+				  frame[1] = m;
+			  }
+			  else{
+				  temp[2] = referenceString[m];
+				  frame[2] = m;
+			  }
+		  }
+
+            }
+
+	    printf("%d\t\t",m+1);
+            for(n = 0; n < frames; n++)
+            {
+                  //printf("%d\t\t",n+1);
+		  if(temp[n] == -1)
+			  printf("%c\t", ' ');
+		  else
+		  	printf("%d\t", temp[n]);
+            }
+	    printf("%c\n", fault);
+      }
+	    printf("Number of page faults : %d times\n", pageFaults);
+	    printf("**************************************************\n");
 
 	return 0;
 }
@@ -70,6 +152,7 @@ int opti_page(int frames, int pages, int *referenceString){
 	    else if(s == 0){
 		    int i = 0;
 		    for(int a = 0;a<frames;a++){
+			    frame[a] = 0;
 		    	for(i = m+1;i<pages;i++){
 			    if(referenceString[i] == temp[a]){
 				    frame[a] = i;
@@ -77,13 +160,27 @@ int opti_page(int frames, int pages, int *referenceString){
 			}
 			if(frame[a] == 0) frame[a] = pages;
 		    }
+		   
 		    if(frame[0]<frame[1]){
-			    if(frame[1]<frame[2])
+			    if(frame[1]<frame[2]){
 				    temp[2] = referenceString[m];
-			    else
+				    frame[2] = 0;
+			    }
+			    else{
 				    temp[1] = referenceString[m];
+				    frame[1] = 0;
+			    }
 		    }
-		    else temp[0] = referenceString[m];
+		    else {
+			    if(frame[0]<frame[2]){
+				    temp[2] = referenceString[m];
+				    frame[2] = 0;
+			    }
+			    else{
+			    	temp[0] = referenceString[m];
+				frame[0] = 0;
+			    }
+		    }
 	    }
 
 	    printf("%d\t\t",m+1);
@@ -176,6 +273,7 @@ int main()
 
       opti_page(frames, pages, referenceString);
       fifo_page(frames, pages, referenceString);
+      lru_page(frames, pages, referenceString);
       return 0;
 }
 
